9216a9d998060434f3e02bc230777998d8321a5f
OrmeSile <95030000+OrmeSile@users.noreply.github.com>
2022-05-17 13:38:05 +0200

    done 10.10

--- rate-repository-app/src/theme.js
+++ rate-repository-app/src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native';
+
 const theme = {
   colors: {
     textPrimary: '#24292e',
@@ -5,14 +7,18 @@ const theme = {
     primary: '#0366d6',
     appBarBackgroundColor: '#24292e',
     mainBackgroundColor: '#e1e4e8',
-    errorColor: '#d73a4a'
+    errorColor: '#d73a4a',
   },
   fontSizes: {
     body: 14,
     subheading: 16,
   },
   fonts: {
-    main: 'System',
+    main: Platform.select({
+      android: 'Roboto',
+      ios: 'Arial',
+      default: 'System',
+    }),
   },
   fontWeights: {
     normal: '400',
